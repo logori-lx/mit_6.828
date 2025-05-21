493e8296fb061a33b46c091d033bdde51f18fcbb
logori-lx <1731477306@qq.com>
2025-05-21 20:08:23 +0800

    [Lab1] Finish pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,53 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int main(void){
+    int p1[2],p2[2];
+    if(pipe(p1) < 0){
+        LOG_ERROR("pipe failed!\n");
+        exit(1);
+    }
+    if(pipe(p2) < 0){
+        LOG_ERROR("pipe failed!\n");
+        exit(1);
+    }
+    int pid = fork();
+    // child
+    if(pid == 0){
+        int pid = getpid();
+        char data = 'b';
+        close(p1[0]);
+        close(p2[1]);
+        if(write(p1[1],&data,1) < 0){
+            LOG_ERROR("write failed!\n");
+            exit(1);
+        }
+        if(read(p2[0],&data, 1) < 0){
+            LOG_ERROR("read failed!\n");
+            exit(1);
+        }else{
+            printf("%d: received pong\n",pid);
+            exit(0);
+        }
+    //parent
+    }else if(pid > 0){
+        int pid = getpid();
+        char data = 'b';
+        close(p1[1]);
+        close(p2[0]);
+        if(read(p1[0],&data, 1) < 0){
+            LOG_ERROR("read failed!\n");
+            exit(1);
+        }else{
+            printf("%d: received ping\n",pid);
+            exit(0);
+        }
+        if(write(p2[1],&data,1) < 0){
+            LOG_ERROR("write failed!\n");
+            exit(1);
+        }
+    }else{
+        LOG_ERROR("fork failed\n");
+        exit(1);
+    }
+}
