9dc82c06b1dc89ba3fc394d9ecf1d49522153e84
logori-lx <1731477306@qq.com>
2025-09-07 15:29:31 +0800

    [Lab1] Finish primes

--- user/primes.c
+++ user/primes.c
@@ -1,42 +1,62 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-
-void dealing(int* left_pipe){
+void dealing(int *left_pipe)
+{
     close(left_pipe[1]);
     int prime;
-    if(read(left_pipe[0], &prime, 4) < 0){
+    int nread = read(left_pipe[0], &prime, 4);
+    if (nread < 0)
+    {
         LOG_ERROR("Read failed!");
         close(left_pipe[0]);
         exit(1);
     }
-    printf("prime %d\n", prime);
+    else if (nread < 4)
+    {
+        close(left_pipe[0]);
+        exit(0);
+    }
+    else
+    {
+        printf("prime %d\n", prime);
+    }
+
     int right_pipe[2];
     int ret = pipe(right_pipe);
-    if(ret < 0){
+    if (ret < 0)
+    {
         close(left_pipe[0]);
         LOG_ERROR("Right pipe create failed!, errno: %d", ret);
         exit(1);
     }
     int pid = fork();
-    if(pid > 0){
+    if (pid > 0)
+    {
         int n;
         close(right_pipe[0]);
-        while(read(left_pipe[0], &n, 4) > 0){
-            if(n % prime != 0){
-                if(write(right_pipe[1], &n, 4) < 0){
+        while (read(left_pipe[0], &n, 4) > 0)
+        {
+            if (n % prime != 0)
+            {
+                if (write(right_pipe[1], &n, 4) < 0)
+                {
                     LOG_ERROR("Write failed!");
-                    close(right_pipe[1]);
-                    close(left_pipe[0]);
+                    break;
                 }
             }
         }
         close(right_pipe[1]);
         close(left_pipe[0]);
         wait(0);
-    }else if(pid == 0){
+    }
+    else if (pid == 0)
+    {
+        close(left_pipe[0]);
         dealing(right_pipe);
-    }else{
+    }
+    else
+    {
         close(left_pipe[0]);
         close(right_pipe[0]);
         close(right_pipe[1]);
@@ -45,29 +65,36 @@ void dealing(int* left_pipe){
     }
 }
 
-
-
-
-int main(void){
+int main(void)
+{
     int left_pipe[2];
-    if(pipe(left_pipe) < 0){
+    if (pipe(left_pipe) < 0)
+    {
         LOG_ERROR("Pipe create failed!");
         exit(1);
     }
     int pid = fork();
-    if(pid > 0){
+    if (pid > 0)
+    {
         close(left_pipe[0]);
-        for(int i = 2; i <= 280; i++){
-            if(write(left_pipe[1], &i, 4) < 0){
+        for (int i = 2; i <= 280; i++)
+        {
+            if (write(left_pipe[1], &i, 4) < 0)
+            {
                 LOG_ERROR("Write failed!");
                 close(left_pipe[1]);
                 exit(1);
             }
         }
+        close(left_pipe[1]);
         wait(0);
-    }else if(pid == 0){
+    }
+    else if (pid == 0)
+    {
         dealing(left_pipe);
-    }else{
+    }
+    else
+    {
         LOG_ERROR("Fork failed!");
         close(left_pipe[0]);
         close(left_pipe[1]);
